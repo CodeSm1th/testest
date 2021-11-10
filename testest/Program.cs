60c428bc15190070f1ac0db6f8022223b447035c
mathi <mathi@LAPTOP-N1V5F1CN>
2021-11-10 09:43:33 +0100

    Add project files.

---
+++ testest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
